4bb275d027a452fafada9fac44e0e72c571129a2
jaerrib <jaerrib@tutanota.com>
2024-09-17 12:00:51 -0400

    Update debug setting

--- django_project/settings.py
+++ django_project/settings.py
@@ -27,7 +27,7 @@ env.read_env()
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "driving-hours.fly.dev",
